ffeca309a963f62c9c9017e6a7f2e7dd0e46cd9d
Akanksha Mahajan <95615302+Akanksha1104@users.noreply.github.com>
2023-01-28 12:20:55 +0530

    Update BankFinal.py

--- BankFinal.py
+++ BankFinal.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 
 # loading the saved model
-loaded_model = pickle.load(open('model.pkl', 'rb'))
+loaded_model = pickle.load(open('model.rar', 'rb'))
 
 
 # creating a function for Prediction
